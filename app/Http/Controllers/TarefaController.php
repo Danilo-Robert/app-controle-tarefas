b77cf10eae5158e22a4813370c40d87cb9058555
danilo-robert <99danilorobert@gmail.com>
2026-05-07 13:22:00 -0400

    implementado middleware auth

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    /*
+    Indicando que só será possível acessar as rotas do controllar com uma sessão válida
+    */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return 'Deu certo!';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TarefaController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -14,3 +16,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tarefa', TarefaController::class);
